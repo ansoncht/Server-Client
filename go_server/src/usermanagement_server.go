12f8f77d47376dffd544debc77215e56f2c4a995
Anson Chung <ansoncht@gmail.com>
2022-07-15 19:38:04 -0700

    Created protos and minmal server

---
+++ go_server/src/usermanagement_server.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"log"
+	"math"
+	"math/rand"
+	"net"
+
+	pb "github.com/ansoncht/server-client/protos"
+
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":5001"
+)
+
+type UserManagementServer struct {
+	pb.UnimplementedUserManagementServer
+}
+
+func (s *UserManagementServer) CreateNewUser(ctx context.Context, in *pb.NewUser) (*pb.User, error) {
+	// log received request's name and age
+	log.Printf("Received name: %v age: %v", in.GetName(), in.GetAge())
+	var user_id int32 = int32(rand.Intn(math.MaxInt32))
+
+	// return created user
+	return &pb.User{Name: in.GetName(), Age: in.GetAge(), Id: user_id}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("Failed to listen %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterUserManagementServer(s, &UserManagementServer{})
+	log.Printf("Server listening at %v", lis)
+
+	err = s.Serve(lis)
+	if err != nil {
+		log.Fatalf("Failed to serve %v", err)
+	}
+
+}
